021a465a5851724c1d8cc668c6f5f0b507783e31
Ace Olszowka <aolszowka@gmail.com>
2019-07-08 10:35:15 -0600

    Added Get-SolutionFileCounts.ps1 to get the count of projects referenced in a given Visual Studio Solution

---
+++ VisualStudio/Get-SolutionFileCounts.ps1
@@ -0,0 +1,30 @@
+param
+(
+    [Parameter(Mandatory = $true)]
+    [string]$DirectoryToScan
+)
+
+# Basically perform all the logic in C#; we only use powershell because it
+# allows us to have C# Code under version control that is not a committed binary
+$msBuildAssembly = [Reflection.Assembly]::Load("Microsoft.Build, Version=14.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a, processorArchitecture=MSIL")
+
+Add-Type @'
+using Microsoft.Build.Construction;
+
+public static class SolutionUtilities
+{
+    public static int ProjectCount(string solutionFile)
+    {
+        SolutionFile sln = SolutionFile.Parse(solutionFile);
+        return sln.ProjectsByGuid.Count;
+    }
+}
+'@ -ReferencedAssemblies $msBuildAssembly
+
+# Find all SLN Files
+$slnFiles = Get-ChildItem -Path $DirectoryToScan -Filter *.sln -Recurse
+
+foreach($slnFile in $slnFiles)
+{
+   Write-Host "$($slnFile.FullName)`t$([SolutionUtilities]::ProjectCount($slnFile.FullName))"
+}
