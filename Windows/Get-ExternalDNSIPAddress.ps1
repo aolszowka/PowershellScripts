7366b7524ec6363bd8abe3f2d4a4f866e39c64a2
Ace Olszowka <aolszowka@gmail.com>
2022-03-31 14:28:28 -0600

    Grab External IP Using Amazon Web Services

--- Windows/Get-ExternalDNSIPAddress.ps1
+++ Windows/Get-ExternalDNSIPAddress.ps1
@@ -2,6 +2,7 @@
 # address pointed at by a particular hostname exporting into a CSV, updating
 # only when the address changes.
 
+$targetHostName = 'example.com'
 $ipAddressLogFile = "$PSScriptRoot\IPAddresses.csv"
 $previousAddresses = $null
 
@@ -10,23 +11,34 @@ if (Test-Path -Path $ipAddressLogFile) {
     [System.Collections.Generic.List[PSCustomObject]]$previousAddresses = Import-Csv -Path $ipAddressLogFile
 }
 
-$externalIP = Resolve-DnsName -Name 'example.com' | Where-Object { $_.Type -eq 'A' }
+$externalIP = 'Failed to get external IP from https://checkip.amazonaws.com'
+$externalIPWebRequest = Invoke-WebRequest -Uri 'https://checkip.amazonaws.com'
+if($null -ne $externalIPWebRequest -and $externalIPWebRequest.StatusCode -eq 200) {
+    $externalIP = [string]::new($externalIPWebRequest.Content)
+    $externalIP = $externalIP.Replace("`n", [string]::Empty)
+}
+
+$dnsIP = Resolve-DnsName -Name $targetHostName | Where-Object { $_.Type -eq 'A' }
 
 if ($null -eq $previousAddresses) {
     # This is the first time we're initializing this file go ahead and create it
     $previousAddresses = [PSCustomObject]@{
-        IPAddress = $externalIP.IPAddress
+        HostName = $targetHostName
+        DNSIP = $dnsIP.IPAddress
+        IPAddress = $externalIP
         FirstSeen = Get-Date
     }
 }
 else {
     $mostCurrentAddress = $previousAddresses[0]
-    if ($mostCurrentAddress.IPAddress -eq $externalIP.IPAddress) {
+    if ($mostCurrentAddress.IPAddress -eq $externalIP -and $mostCurrentAddress.DNSIP -eq $dnsIP.IPAddress) {
         # IP Address has not changed do nothing
     }
     else {
         $currentIPAddress = [PSCustomObject]@{
-            IPAddress = $externalIP.IPAddress
+            HostName = $targetHostName
+            DNSIP = $dnsIP.IPAddress
+            IPAddress = $externalIP
             FirstSeen = Get-Date
         }
         $previousAddresses.Insert(0, $currentIPAddress)
