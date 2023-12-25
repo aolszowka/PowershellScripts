74ae070adddb8b8800b7482f328bedf3701f5dad
Ace Olszowka <aolszowka@gmail.com>
2023-12-25 15:41:23 -0700

    Add Support for Diarization via WhisperX

--- WhisperAI/Invoke-BatchTranscriptionSRT.ps1
+++ WhisperAI/Invoke-BatchTranscriptionSRT.ps1
@@ -30,6 +30,36 @@ function ConvertTo-SRT {
     Write-Host "Converted [$SourceFile]; Took [$executionTime]"
 }
 
+function ConvertTo-JsonDiarize {
+    param (
+        [string]$SourceFile,
+        [string]$DestinationFile = "$SourceFile.json",
+        [int]$MinSpeakers = 2,
+        [int]$MaxSpeakers = 2
+    )
+
+    # Do not use an initial prompt as this does not work for WhisperX
+    $uri = "http://mp80.localdomain:9001/asr?task=transcribe&language=en&encode=true&output=json&diarize=true&min_speakers=$MinSpeakers&max_speakers=$MaxSpeakers"
+    $headers = @{
+        'accept'       = 'application/json'
+        'Content-Type' = 'multipart/form-data'
+    }
+    $form = @{
+        'audio_file' = Get-Item -Path $SourceFile
+        'type'       = 'audio/x-flac'
+    }
+
+    $duration = Get-Duration -SourceFile $SourceFile
+    Write-Host "Creating Diarized JSON for [$SourceFile] at [$DestinationFile] with Duration of [$duration] at [$([System.DateTime]::Now)]"
+
+    $executionTime = Measure-Command -Expression {
+        $response = Invoke-RestMethod -Uri $uri -Headers $headers -Form $form -Method 'POST'
+        $response | ConvertTo-Json -Depth 100 | Out-File -FilePath $DestinationFile
+    }
+
+    Write-Host "Converted [$SourceFile]; Took [$executionTime]"
+}
+
 function Get-Duration {
     param (
         [string]$SourceFile
@@ -53,14 +83,39 @@ function Get-Duration {
     return $duration
 }
 
-$sourceFiles = Get-ChildItem -Path $targetDirectory -Filter *.flac -Recurse | Select-Object -ExpandProperty FullName
+function Invoke-SRTConversion {
+    param (
+        $sourceFiles
+    )
 
-foreach ($sourceFile in $sourceFiles) {
-    if (Test-Path -Path "$sourceFile.srt") {
-        Write-Warning "SRT Exists for [$sourceFile]. Skipping..."
-    }
-    else {
-        ConvertTo-SRT -SourceFile $sourceFile
+    foreach ($sourceFile in $sourceFiles) {
+        if (Test-Path -Path "$sourceFile.srt") {
+            Write-Warning "SRT Exists for [$sourceFile]. Skipping..."
+        }
+        else {
+            ConvertTo-SRT -SourceFile $sourceFile
+        }
     }
 }
 
+function Invoke-DiarizedJSONConversion {
+    param (
+        $sourceFiles
+    )
+
+    foreach ($sourceFile in $sourceFiles) {
+        if (Test-Path -Path "$sourceFile.json") {
+            Write-Warning "JSON Exists for [$sourceFile]. Skipping..."
+        }
+        else {
+            ConvertTo-JsonDiarize -SourceFile $sourceFile
+        }
+    }
+}
+
+
+$sourceFiles = Get-ChildItem -Path $targetDirectory -Filter *.flac -Recurse | Select-Object -ExpandProperty FullName
+Invoke-SRTConversion -sourceFiles $sourceFiles
+
+#$sourceFiles = Get-ChildItem -Path 'C:\TranscriptionDiarized' -Recurse -Filter *.flac | Select-Object -ExpandProperty FullName
+#Invoke-DiarizedJSONConversion -sourceFiles $sourceFiles
