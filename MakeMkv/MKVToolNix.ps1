fcd34f93326cbf7a5f63f5cefd6203d741ec8856
Ace Olszowka <aolszowka@gmail.com>
2023-12-26 20:31:28 -0700

    Make Season and Episode Naming Optional

--- MakeMkv/MKVToolNix.ps1
+++ MakeMkv/MKVToolNix.ps1
@@ -8,16 +8,31 @@ function Get-ItemListing {
         $TargetPath
     )
     process {
+        $includeSeasonAndEpisode = $true
         $fileNames = Get-ChildItem -LiteralPath $TargetPath -Recurse -Filter '*.mkv' | Select-Object -ExpandProperty FullName
         # Start to build up the title based on the file naming convention
         foreach ($fileName in $fileNames) {
             $title = [System.IO.Path]::GetFileNameWithoutExtension($fileName)
             $title = $title.Replace('.', ' ')
-            $seasonEpisodeString = [System.Text.RegularExpressions.Regex]::Match($title, 'S[0-9]+E[0-9]+')
-            $title = $title.Insert($seasonEpisodeString.Index, '- ')
-            # Calculate Again to Add it After
-            $seasonEpisodeString = [System.Text.RegularExpressions.Regex]::Match($title, 'S[0-9]+E[0-9]+')
-            $title = $title.Insert($seasonEpisodeString.Index + $seasonEpisodeString.Length, ' -')
+            if ($includeSeasonAndEpisode -eq $true) {
+                # When a Season and Episode value are present attempt to name
+                # the file with that information in the title.
+                $seasonEpisodeString = [System.Text.RegularExpressions.Regex]::Match($title, 'S[0-9]+E[0-9]+')
+                # Add to to the end of the dash because that won't mess up the index from above
+                $title = $title.Insert($seasonEpisodeString.Index + $seasonEpisodeString.Length, ' -')
+                # Then add it before
+                $title = $title.Insert($seasonEpisodeString.Index, '- ')
+            }
+            else {
+                # We also need to support where we've got Absolute Ordering (No
+                # Season) hence the Season is optional.
+                $title = [System.Text.RegularExpressions.Regex]::Replace($title, '(S[0-9]+)?E[0-9]+', [string]::Empty)
+                $title = $title.Trim()
+                $title = $title.Trim('-')
+                # Trim Whitespace again because removing the `-` might have
+                # cleared us up.
+                $title = $title.Trim()
+            }
             $title = "$title"
 
             [PSCustomObject]@{FileName = $fileName; Title = $title }
