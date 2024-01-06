f1a361584a289c5d7caef039403dc4b24949c003
Ace Olszowka <aolszowka@gmail.com>
2024-01-06 11:13:24 -0700

    Sort by Title
    Improve Invoke-SeasonEpisodeNaming to sort by title (IE C2_T2 would now come before A1_T5).

--- MakeMkv/Invoke-SeasonEpisodeNaming.ps1
+++ MakeMkv/Invoke-SeasonEpisodeNaming.ps1
@@ -1,8 +1,8 @@
 # The intent of this script is to help with mass renaming files ripped via
 # MakeMKV to the format S00E00 by giving the Season and Start Episode and then
-# assuming that the order of the files (IE C_T01.mkv, C_T02.mkv) will be the
-# order of the Episodes. It will also rename the parent folder to the format
-# `Season.SeasonNumber` to assist Filebot.
+# assuming that the order of the files based on the Title (IE C_T01.mkv,
+# C_T02.mkv) will be the order of the Episodes. It will also rename the parent
+# folder to the format `Season.SeasonNumber` to assist Filebot.
 function Invoke-SeasonEpisodeNaming {
     [CmdletBinding()]
     param (
@@ -20,10 +20,25 @@ function Invoke-SeasonEpisodeNaming {
         if (Test-Path $Path) {
             $filesInFolder = Get-ChildItem -LiteralPath $Path | Sort-Object -Property Name | Select-Object -ExpandProperty FullName
 
+            # We need to sort this by the Title
+            [System.Collections.Generic.SortedDictionary[string, string]]$titleSort = [System.Collections.Generic.SortedDictionary[string, string]]::new()
+            foreach ($file in $filesInFolder) {
+                $fileNameWithoutExtension = [System.IO.Path]::GetFileNameWithoutExtension($file)
+                $title = [System.Text.RegularExpressions.Regex]::Match($fileNameWithoutExtension, '[A-Z][0-9]_t(?<TitleNumber>[0-9]+)').Groups['TitleNumber'].Value
+                if ($titleSort.ContainsKey($title)) {
+                    Write-Error "Duplicate Titles Found; This Tool Cannot Be Used"
+                    exit
+                }
+                else {
+                    $titleSort.Add($title, $file)
+                }
+            }
+
             $currentEpisode = [int]::Parse($StartEpisode)
 
             # Rename all the files in S00E00 Format
-            foreach ($file in $filesInFolder) {
+            foreach ($fileKvp in $titleSort.GetEnumerator()) {
+                $file = $fileKvp.Value
                 $parentFolder = [System.IO.Path]::GetDirectoryName($file)
                 $fileExtension = [System.IO.Path]::GetExtension($file)
                 $newName = "S$($Season.PadLeft(2,'0'))E$($currentEpisode.ToString().PadLeft(2,'0'))$fileExtension"
